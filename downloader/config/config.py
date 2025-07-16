748c1bf4f3dcda3b7c7d83f3a80c8f28b67f5d8d
Hailo Tech <hailo@had-1004-tns.qb.hailotech>
2025-07-16 19:29:53 +0300

    update downloader

--- downloader/config/config.py
+++ downloader/config/config.py
@@ -2,7 +2,7 @@ import platform
 from pathlib import Path
 
 VERSION = "v5.0"
-MODEL_ZOO_VERSION = "v2.12"
+MODEL_ZOO_VERSION = "v5.0"
 
 CONFIG_PATH = Path(__file__).parent
 DOWNLOADER_PATH = CONFIG_PATH.parent
@@ -10,32 +10,7 @@ ROOT_PATH = DOWNLOADER_PATH.parent
 REQUIREMENTS_PATH = CONFIG_PATH / "requirements"
 
 BUCKETS_FILE = CONFIG_PATH / "buckets.json"
-REQUIREMENTS_FILES = ["general/cascading_networks.json",
-                      "general/century.json",
-                      "general/depth_estimation.json",
-                      "general/detection.json",
-                      "general/face_recognition.json",
-                      "general/instance_segmentation.json",
-                      "general/license_plate_recognition.json",
-                      "general/multistream_detection.json",
-                      "general/multi_device.json",
-                      "general/python.json",
-                      "general/re_id.json",
-                      "general/tiling.json",
-                      "x86/x86_sanity.json",
-                      "x86/x86_multistream_detection.json",
-                      "x86/x86_century.json",
-                      "rockchip/detection.json",
-                      "rockchip/license_plate_recognition.json",
-                      "rockchip/multistream_detection.json",
-                      "rockchip/tiling.json",
-                      "rpi/rpi_depth_estimation.json",
-                      "rpi/rpi_detection.json",
-                      "imx8/imx8_cascading_networks.json",
-                      "imx8/imx8_depth_estimation.json",
-                      "imx8/imx8_detection.json",
-                      "imx8/imx8_license_plate_recognition.json",
-                      "imx8/imx8_multistream_detection.json"]
+REQUIREMENTS_FILES = ["general/detection.json"]
 
 # Used during the download to decide what the source / destination is dynamically
 RESERVED_DOWNLOADER_KEYWORDS = {
